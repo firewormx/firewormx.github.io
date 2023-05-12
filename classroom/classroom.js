e9708ad9f67c4375a832ef050d207bb6be3bf0e8
hotaru <huyuchen0810@gmail.com>
2023-05-12 22:45:25 +0900

    update classroom

---
+++ classroom/classroom.js
@@ -0,0 +1,34 @@
+import Stats from "classroom/stats.js";
+
+let gradesForm = document.querySelector("#grades-form");
+let yourGrade = document.querySelector("#your-grade");
+
+let grades = [14, 9, 13, 15, 18];
+
+function render(grades) {
+    let stats = new Stats(grades);
+    console.log("Grades: " + grades.join(', '));
+    let tbody = document.querySelector("#stats-table tbody");
+    tbody.innerHTML = `<tr>
+    <td>${stats.getNumberOfGrades()}</td>
+    <td>${stats.getFirstGrade()}</td>
+    <td>${stats.getLastGrade()}</td>
+    </tr>`;
+
+    let tbody2 = document.querySelector("#stats-table2 tbody");
+    tbody2.innerHTML = `<tr>
+    <td>${stats.getSumOfGrades()}</td>
+    <td>${stats.getAverageGrade()}</td>
+    <td>${stats.exportCsv()}</td>
+    </tr>`;
+}
+
+gradesForm.addEventListener("submit", event => {
+    event.preventDefault();
+    let newGrade = Number.parseInt(yourGrade.value, 10);
+    grades.push(newGrade);
+    yourGrade.value = "";
+    render(grades);
+})
+
+render(grades);

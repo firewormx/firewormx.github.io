732a280debc2c4f0bc7456a9ed609fcfb7707e79
hotaru <huyuchen0810@gmail.com>
2023-05-12 23:44:14 +0900

    merge stats.js in classroom.js

--- classroom/classroom.js
+++ classroom/classroom.js
@@ -1,4 +1,38 @@
-import Stats from "stats.js";
+export default class Stats {
+    
+    constructor(grades){
+    this.grades = grades;
+    }
+    getNumberOfGrades()
+    {
+        return this.grades.length;
+    }
+    
+    getFirstGrade()
+    {return this.grades[0];
+    
+    }
+    getLastGrade(){
+    return this.grades[this.grades.length -1];
+    }
+    getSumOfGrades(){
+    let sum = 0
+    this.grades.forEach(function(grade){
+        sum = sum + grade;
+    })
+    return sum;
+    }
+    getAverageGrade(){
+        let sum = 0;
+        this.grades.forEach(function(grade){
+            sum = sum + grade;
+        })
+        return sum/this.grades.length;
+    }
+    exportCsv()
+    {return this.grades.join(", " );
+    
+    }}
 
 let gradesForm = document.querySelector("#grades-form");
 let yourGrade = document.querySelector("#your-grade");

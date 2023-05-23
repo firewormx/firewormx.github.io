42d996cfdb7b385a9302889f313e6b1be2732e72
hotaru <huyuchen0810@gmail.com>
2023-05-23 14:11:55 +0900

    update "import csv"

---
+++ learnprogramming/import_csv/import_csv.js
@@ -0,0 +1,25 @@
+export default class Tasks {
+    constructor() {
+        this.tasks = [];
+    }
+
+    /** @param {string} csvString */
+    importCsv(csvString) {
+   this.tasks=csvString.split(", ");
+    }
+    getCount(){  
+        return this.tasks.length;
+    }
+getFirst(){
+return this.tasks[0];
+}
+getLast(){
+return this.tasks[this.tasks.length -1];
+}
+getUnformattedTasks(){
+return this.tasks.map(function(task){
+return task.toLowerCase()
+}).join(", ");
+
+}
+}

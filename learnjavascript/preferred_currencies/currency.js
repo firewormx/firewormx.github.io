3105d685267e3a000ece2c997c112c09e9d1723f
firewormx <huyuchen0810@gmail.com>
2025-05-28 16:59:20 +0900

    update preferred_currencies in typescript

--- learnjavascript/preferred_currencies/currency.js
+++ learnjavascript/preferred_currencies/currency.js
@@ -1,3 +1,4 @@
+//@ts-check
 export const getSelectedCurrencies = () => {
     const activeCard= [...document.querySelectorAll(".cards .card.active")]
      return activeCard.map(activeCard=> activeCard.textContent);
--- learnjavascript/preferred_currencies/index.ts
+++ learnjavascript/preferred_currencies/index.ts
@@ -1,18 +1,18 @@
 import {getSelectedCurrencies} from "./currency.js";
 
-document.querySelectorAll(".cards .card")
-.forEach(card => {
+const cards = document.querySelectorAll<HTMLButtonElement>(".cards .card");
+cards.forEach(card => {
     card.addEventListener("click", event => {
-        event.currentTarget.classList.toggle("active");
+        (event.currentTarget as HTMLElement).classList.toggle("active");
         console.log(getSelectedCurrencies());
     });
 })
 
-const cards_m= document.querySelectorAll(".cards_m .card_m");
+const cards_m= document.querySelectorAll<HTMLButtonElement>(".cards_m .card_m");
 cards_m.forEach(card_m =>{
      card_m.addEventListener("click", event=>{
         document.querySelector(".cards_m .card_m.active")?.classList.remove("active");
-        event.currentTarget.classList.add("active");
+        (event.currentTarget as HTMLElement).classList.add("active");
      })
 })
 

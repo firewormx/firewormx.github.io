09c550cc25be3c844ef09ed283dca3e5615af66a
firewormx <huyuchen0810@gmail.com>
2025-05-28 18:10:43 +0900

    update preferred_currencies

--- learnjavascript/preferred_currencies/currency.js
+++ learnjavascript/preferred_currencies/currency.js
@@ -1,8 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getSelectedCurrencies = void 0;
-var getSelectedCurrencies = function () {
-    var activeCards = Array.from(document.querySelectorAll(".cards .card.active"));
-    return activeCards.map(function (card) { var _a; return (_a = card.textContent) !== null && _a !== void 0 ? _a : ""; });
-};
-exports.getSelectedCurrencies = getSelectedCurrencies;
+export function getSelectedCurrencies() {
+    const activeCards = Array.from(document.querySelectorAll(".cards .card.active"));
+    return activeCards.map(card => card.textContent ?? "");
+}
--- learnjavascript/preferred_currencies/main.ts
+++ learnjavascript/preferred_currencies/main.ts
@@ -1,18 +1,18 @@
-import {getSelectedCurrencies} from "./currency.ts";
+import { getSelectedCurrencies } from "./currency";
 
-const cards = document.querySelectorAll<HTMLButtonElement>(".cards .card");
+const cards = document.querySelectorAll<HTMLDivElement>(".cards .card");
 cards.forEach(card => {
     card.addEventListener("click", event => {
         (event.currentTarget as HTMLElement).classList.toggle("active");
         console.log(getSelectedCurrencies());
     });
-})
+});
 
-const cards_m= document.querySelectorAll<HTMLButtonElement>(".cards_m .card_m");
-cards_m.forEach(card_m =>{
-     card_m.addEventListener("click", event=>{
+const cards_m = document.querySelectorAll<HTMLDivElement>(".cards_m .card_m");
+cards_m.forEach(card_m => {
+    card_m.addEventListener("click", event => {
         document.querySelector(".cards_m .card_m.active")?.classList.remove("active");
         (event.currentTarget as HTMLElement).classList.add("active");
-     })
-})
+    });
+});
 

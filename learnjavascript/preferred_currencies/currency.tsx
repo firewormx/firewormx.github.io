cd5492293b82dd58bff31bf650402e7207c3e3b2
firewormx <huyuchen0810@gmail.com>
2025-05-28 17:14:48 +0900

    Revert "update preferred_currencies in typescript"
    This reverts commit 3105d685267e3a000ece2c997c112c09e9d1723f.

--- learnjavascript/preferred_currencies/currency.tsx
+++ learnjavascript/preferred_currencies/currency.tsx
@@ -1,4 +1,3 @@
-//@ts-check
 export const getSelectedCurrencies = () => {
     const activeCard= [...document.querySelectorAll(".cards .card.active")]
      return activeCard.map(activeCard=> activeCard.textContent);
